a12958c7ef7d8cffecbc799d83df1e5c841cf4af
Kong Lee <kong.lee@vinova.com.sg>
2022-09-12 13:16:38 +0700

    update

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function createDefaultWindow() {
   return win;
 }
 autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
+  sendStatusToWindow('Checking for updateeeeeee...');
 })
 autoUpdater.on('update-available', (info) => {
   sendStatusToWindow('Update available.');
